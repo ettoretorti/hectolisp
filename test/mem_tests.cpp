5130c54e3d6020c45e3e27d32bcd466f305e34f3
Ettore Torti <ettore.torti@student.manchester.ac.uk>
2016-07-08 16:43:44 +0100

    Add CMakelists.txt and tests

---
+++ test/mem_tests.cpp
@@ -0,0 +1,59 @@
+#include "Scheme.h"
+#include "SchemeSecret.h"
+
+#include <gtest/gtest.h>
+
+TEST(Memory, CheckAllocation) {
+	scm_init_mem();
+	
+	Expr* i = scm_mk_int(42);
+	EXPECT_TRUE(scm_is_atom(i));
+	EXPECT_TRUE(scm_is_int(i));
+	EXPECT_EQ(scm_ival(i), 42);
+
+	Expr* r = scm_mk_real(42.0);
+	EXPECT_TRUE(scm_is_atom(i));
+	EXPECT_TRUE(scm_is_real(r));
+	EXPECT_EQ(scm_rval(r), 42.0);
+
+	Expr* c = scm_mk_char('E');
+	EXPECT_TRUE(scm_is_atom(i));
+	EXPECT_TRUE(scm_is_char(c));
+	EXPECT_EQ(scm_cval(c), 'E');
+
+	Expr* sym = scm_mk_symbol("symbol");
+	EXPECT_TRUE(scm_is_atom(i));
+	EXPECT_TRUE(scm_is_symbol(sym));
+	EXPECT_STREQ(scm_sval(sym), "symbol");
+
+	Expr* str = scm_mk_string("string");
+	EXPECT_TRUE(scm_is_atom(i));
+	EXPECT_TRUE(scm_is_string(str));
+	EXPECT_STREQ(scm_sval(str), "string");
+
+	Expr* p = scm_mk_pair(i, r);
+	EXPECT_TRUE(scm_is_pair(p));
+	EXPECT_EQ(scm_car(p), i);
+	EXPECT_EQ(scm_cdr(p), r);
+
+	scm_gc();
+}
+
+TEST(Memory, CheckCorruption) {
+	scm_init_mem();
+	
+	Expr* es[500];
+	for(int i=0; i<500; i++) {
+		es[i] = scm_mk_int(i);
+		EXPECT_TRUE(scm_is_int(es[i]));
+		EXPECT_EQ(i, scm_ival(es[i]));
+	}
+	
+	//check for corruption of older allocations by more recent ones
+	for(int i=0; i<500; i++) {
+		EXPECT_TRUE(scm_is_int(es[i]));
+		EXPECT_EQ(i, scm_ival(es[i]));
+	}
+	
+	scm_gc();
+}
---
+++ test/parse_tests.cpp
@@ -0,0 +1,93 @@
+#include "Scheme.h"
+#include "SchemeSecret.h"
+
+#include <gtest/gtest.h>
+
+TEST(Parsing, Atoms) {
+	scm_init_mem();
+	
+	//Expr* i = scm_read("42");
+	//EXPECT_TRUE(scm_is_int(i));
+	//EXPECT_EQ(42, scm_ival(i));
+
+	Expr* r = scm_read("42.0");
+	EXPECT_TRUE(scm_is_real(r));
+	EXPECT_EQ(42.0, scm_rval(r));
+
+	//Expr* c = scm_read("#\\c");
+	//EXPECT_TRUE(scm_is_char(c));
+	//EXPECT_EQ('c', scm_cval(c));
+
+	Expr* t = scm_read("#t");
+	EXPECT_TRUE(scm_is_bool(t));
+	EXPECT_TRUE(scm_bval(t));
+	EXPECT_EQ(TRUE, t);
+	
+	Expr* f = scm_read("#f");
+	EXPECT_TRUE(scm_is_bool(f));
+	EXPECT_FALSE(scm_bval(f));
+	EXPECT_EQ(FALSE, f);
+
+	Expr* str = scm_read("\"string\"");
+	EXPECT_TRUE(scm_is_string(str));
+	EXPECT_STREQ("string", scm_sval(str));
+
+	Expr* sym = scm_read("symbol");
+	EXPECT_TRUE(scm_is_symbol(sym));
+	EXPECT_STREQ("symbol", scm_sval(sym));
+
+	Expr* e = scm_read("()");
+	EXPECT_TRUE(scm_is_pair(e));
+	EXPECT_EQ(EMPTY_LIST, e);
+
+	scm_gc();
+}
+
+TEST(Parsing, Pairs) {
+	scm_init_mem();
+	
+	Expr* a = scm_read("(() . ())");
+	EXPECT_TRUE(scm_is_pair(a));
+	EXPECT_EQ(EMPTY_LIST, scm_car(a));
+	EXPECT_EQ(EMPTY_LIST, scm_cdr(a));
+
+	Expr* b = scm_read("(() . 42.0)");
+	EXPECT_TRUE(scm_is_pair(b));
+	EXPECT_EQ(EMPTY_LIST, scm_car(b));
+	EXPECT_TRUE(scm_is_real(scm_cdr(b)));
+	EXPECT_EQ(42.0, scm_rval(scm_cdr(b)));
+
+	Expr* c = scm_read("(42.0 . ())");
+	EXPECT_TRUE(scm_is_pair(c));
+	EXPECT_TRUE(scm_is_real(scm_car(c)));
+	EXPECT_EQ(42.0, scm_rval(scm_car(c)));
+	EXPECT_EQ(EMPTY_LIST, scm_cdr(c));
+	
+	Expr* d = scm_read("(a . b)");
+	EXPECT_TRUE(scm_is_pair(d));
+	EXPECT_TRUE(scm_is_symbol(scm_car(d)));
+	EXPECT_STREQ("a", scm_sval(scm_car(d)));
+	EXPECT_TRUE(scm_is_symbol(scm_cdr(d)));
+	EXPECT_STREQ("b", scm_sval(scm_cdr(d)));
+
+	Expr* e = scm_read("(a . (b . c))");
+	EXPECT_TRUE(scm_is_pair(e));
+	EXPECT_TRUE(scm_is_symbol(scm_car(e)));
+	EXPECT_STREQ("a", scm_sval(scm_car(e)));
+	Expr* f = scm_cdr(e);
+	EXPECT_TRUE(scm_is_pair(f));
+	EXPECT_TRUE(scm_is_symbol(scm_car(f)));
+	EXPECT_STREQ("b", scm_sval(scm_car(f)));
+	EXPECT_TRUE(scm_is_symbol(scm_cdr(f)));
+	EXPECT_STREQ("c", scm_sval(scm_cdr(f)));
+
+	Expr* g = scm_read("((a . b) . c)");
+
+	scm_gc();
+}
+
+TEST(Parsing, Lists) {
+	scm_init_mem();
+
+	scm_gc();
+}
