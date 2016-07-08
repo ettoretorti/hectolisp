35e2601146fe6b7ccbb576c96049560cb3e09f9d
Ettore Torti <ettore.torti@student.manchester.ac.uk>
2016-07-08 16:40:57 +0100

    Add parsing and printing

---
+++ src/Print.c
@@ -0,0 +1,160 @@
+#include "Scheme.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <assert.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+	char* buf;
+	size_t size;
+	size_t cap;
+} buf;
+
+static jmp_buf excpt;
+
+static void reallocb(buf *b) {
+	assert(b);
+	size_t ncap = (size_t) (b->cap * 1.5);
+	if(ncap < b->cap) {
+		longjmp(excpt, 1);
+	}
+
+	char *nbuf = realloc(b->buf, ncap);
+	if(!nbuf) {
+		longjmp(excpt, 2);
+	}
+
+	b->buf = nbuf;
+	b->cap = ncap;
+}
+
+static void append(buf* b, const char* s) {
+	assert(b);
+	assert(s);
+
+	while(true) {
+		while(*s && (b->size < b->cap)) {
+			b->buf[b->size++] = *s++;
+		}
+		
+		if(*s) reallocb(b);
+		else  return;
+	}
+}
+
+static void print_int(long long ival, buf* b) {
+	assert(b);
+	char lbuf[32];
+	sprintf(lbuf,"%lld", ival);
+
+	append(b, lbuf);
+}
+
+static void print_real(double rval, buf* b) {
+	assert(b);
+	char lbuf[128];
+	sprintf(lbuf, "%f", rval);
+
+	append(b, lbuf);
+}
+
+static void print_bool(bool bval, buf* b) {
+	assert(b);
+
+	append(b, bval ? "#t" : "#f");
+}
+
+static void print_char(char cval, buf* b) {
+	assert(b);
+	//TODO
+}
+
+static void print(Expr* e, buf* b);
+
+static void print_pair(Expr* e, buf* b) {
+	assert(b);
+	assert(e);
+	assert(scm_is_pair(e));
+
+	if(e == EMPTY_LIST) {
+		append(b, "()");
+		return;
+	}
+
+	append(b, "(");
+
+loop:   ;
+	Expr* car = scm_car(e);
+	Expr* cdr = scm_cdr(e);
+
+	print(car, b);
+
+	if(cdr != EMPTY_LIST) {
+		if(scm_is_pair(cdr)) {
+			append(b, " ");
+			e = cdr;
+			goto loop;
+		} else {
+			append(b, " . ");
+			print(cdr, b);
+		}
+	}
+	
+	append(b, ")");
+}
+
+static void print(Expr* e, buf* b) {
+	assert(e);
+	assert(b);
+
+	if(scm_is_pair(e)) {
+		print_pair(e, b);
+		return;
+	}
+
+	switch(e->atom.type) {
+	case INT:
+		print_int(scm_ival(e), b);
+		break;
+	case REAL:
+		print_real(scm_rval(e), b);
+		break;
+	case BOOL:
+		print_bool(scm_bval(e), b);
+		break;
+	case CHAR:
+		print_char(scm_rval(e), b);
+		break;
+	case STRING:
+		//TODO escaping
+		append(b, "\"");
+		append(b, scm_sval(e));
+		append(b, "\"");
+		break;
+	case SYMBOL:
+		append(b, scm_sval(e));
+		break;
+	default:
+		append(b, "#UNKNOWN#");
+		break;
+	}
+}
+
+char* scm_print(Expr* e) {
+	buf b = { .buf = malloc(128), .cap = 128, .size = 0 };
+	
+	int r;
+	if(!(r = setjmp(excpt))) {
+		print(e, &b);
+	} else {
+		free(b.buf);
+		return NULL;
+	}
+	
+	b.buf = realloc(b.buf, b.size+1);
+	b.buf[b.size] = '\0';
+
+	return b.buf;
+}
---
+++ src/Read.c
@@ -0,0 +1,264 @@
+#include "Scheme.h"
+#include <stddef.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+struct Buffer {
+	const char* const s;
+	size_t i;
+	const size_t n;
+};
+typedef struct Buffer Buffer;
+
+static void b_eat_white(Buffer* b) {
+	size_t i = b->i;
+
+	while(b->s[i] != '\0' && isspace(b->s[i])) {
+		i++;
+	}
+	b->i = i;
+}
+
+static inline bool is_bound(char c) {
+	return c == '\0'
+	    || c == '('
+	    || c == ')'
+	    || c == ';'
+	    || c == '\''
+	    || c == '"'
+	    || c == '`'
+	    || c == '|'
+	    || c == ' '
+	    || c == '\t'
+	    || c == '\n';
+}
+
+static void b_eat_til_bound(Buffer* b) {
+	size_t i = b->i;
+
+	while(!is_bound(b->s[i])) {
+		i++;
+	}
+	b->i = i;
+}
+
+static inline char b_get(Buffer* b) {
+	if(b->i < b->n) return b->s[b->i++];
+	else            return '\0';
+}
+
+static inline char b_peek(const Buffer* b) {
+	return b->s[b->i];
+}
+
+static inline void b_unget(Buffer* b) {
+	assert(b->i > 0);
+	b->i--;
+}
+
+static Expr* read_char(Buffer* b) {
+	//TODO
+	return NULL;
+}
+
+static Expr* read_string(Buffer* b) {
+	char c = b_get(b);
+	assert(c == '"');
+
+	//max string length is 10kb
+	char buf[10240];
+	size_t idx = 0;
+
+	while(true) {
+		assert(idx < 10240);
+
+		c = b_get(b);
+		if(c == '\0') return EMPTY_LIST;
+		if(c == '"') {
+			buf[idx] = '\0';
+			break;
+		}
+
+		if(c == '\\') {
+			c = b_get(b);
+			if(c == '\0') return EMPTY_LIST;
+
+			c = c == 'n' ? '\n' :
+			    c == 't' ? '\t' :
+			                c;
+		}
+
+		buf[idx++] = c;
+	}
+
+	return scm_mk_string(buf);
+}
+
+static Expr* read_symbol(Buffer* b) {
+	char buf[10240];
+	size_t idx = 0;
+
+	while(true) {
+		assert(idx < 10240);
+		
+		char c = b_get(b);
+
+		if(is_bound(c)) {
+			b_unget(b);
+			buf[idx] = '\0';
+			return scm_mk_symbol(buf);
+		}
+
+		buf[idx++] = c;
+	}
+}
+
+static Expr* read_int(Buffer* b, int sign) {
+	long long buf = 0;
+
+	while(true) {
+		char c = b_get(b);
+
+		if(is_bound(c)) break; 
+
+		if(isdigit(c)) {
+			buf *= 10;
+			buf += c - '0';
+		} else {
+			b_eat_til_bound(b);
+			return EMPTY_LIST;
+		}
+	}
+
+	return scm_mk_int(buf * sign);
+}
+
+
+static Expr* read_real(Buffer* b, int sign) {
+	double buf = 0.0;
+	double nDecimals = 1.0;
+	bool postDec = false;
+
+	while(true) {
+		char c = b_get(b);
+
+		if(is_bound(c)) {
+			b_unget(b);
+			break;
+		}
+		
+		if(c == '.') {
+			postDec = true;
+			continue;
+		}
+
+		if(!isdigit(c)) {
+			b_eat_til_bound(b);
+			return EMPTY_LIST;
+		}
+
+		if(!postDec) {
+			buf *= 10;
+			buf += c - '0';
+		} else {
+			nDecimals /= 10;
+			buf += ((double) c - '0') * nDecimals;
+		}
+	}
+
+	return scm_mk_real(buf * sign);
+}
+
+static Expr* read_num(Buffer* b, int sign) {
+	assert(isdigit(b_peek(b)));
+
+	//TODO
+	return read_real(b, sign);
+}
+
+static Expr* reade(Buffer* b);
+
+Expr* reade_list(Buffer* b) {
+	char c = b_get(b);
+	assert(c == '(');
+
+	b_eat_white(b);
+	c = b_peek(b);
+
+	if(c == ')') {
+		b_get(b);
+		return EMPTY_LIST;
+	}
+
+	Expr* read = reade(b);
+	Expr* car = scm_mk_pair(read, EMPTY_LIST);
+	Expr* toRet = car;
+
+	while(true) {
+		b_eat_white(b);
+		if(b_peek(b) == '.') {
+			b_get(b);
+			car->pair.cdr = reade(b);
+			b_eat_white(b);
+			if(b_get(b) != ')') {
+				b_eat_til_bound(b);
+				return EMPTY_LIST;
+			}
+			break;
+		} else if(b_peek(b) == ')') {
+			b_get(b);
+			return toRet;
+		} else {
+			read = reade(b);
+			Expr* ncdr = scm_mk_pair(read, EMPTY_LIST);
+			car->pair.cdr = ncdr;
+			car = ncdr;
+		}
+	}
+
+	return toRet;
+}
+
+static Expr* reade(Buffer* b) {
+	b_eat_white(b);
+
+	char cur = b_peek(b);
+	Expr* final = EMPTY_LIST;
+
+	if(cur == '(') {
+		final = reade_list(b);
+	} else if(isdigit(cur)) {
+		final = read_num(b, 1);
+	} else if(cur == '-') {
+		b_get(b);
+		char c = b_peek(b);
+
+		if(isdigit(c)) {
+			final = read_num(b, -1);
+		} else {
+			b_unget(b);
+			final = read_symbol(b);
+		}
+	} else if(isalpha(cur)) {
+		final = read_symbol(b);
+	} else if(cur == '"') {
+		final = read_string(b);
+	} else if(cur == '#') {
+		b_get(b);
+		char c = b_get(b);
+		
+		final = c == 't'  ? TRUE :
+			c == 'f'  ? FALSE :
+			c == '\\' ? read_char(b) :
+			            EMPTY_LIST;
+	}
+
+	return final;
+}
+
+Expr* scm_read(const char* in) {
+	Buffer b = { .s = in, .i = 0, .n = strlen(in) };
+
+	return reade(&b);
+}
