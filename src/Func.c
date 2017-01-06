04aacb954be9381c161de800c0906b0721175c30
Ettore Torti <ettoretorti@gmail.com>
2017-01-06 18:03:34 +0100

    Add some number predicates

--- src/Func.c
+++ src/Func.c
@@ -2,7 +2,74 @@
 
 #include <assert.h>
 
-Expr* add(Expr* args) {
+static Expr* number(Expr* args) {
+	assert(args);
+
+	if(args == EMPTY_LIST) return scm_mk_error("No args passed to number? (expected 1)");
+
+	Expr* fst = scm_car(args);
+	Expr* rst = scm_cdr(args);
+
+	if(rst != EMPTY_LIST) return scm_mk_error("Too many args passed to number? (expected 1)");
+
+	return (scm_is_int(fst) || scm_is_real(fst)) ? TRUE : FALSE;
+}
+
+static Expr* integer(Expr* args) {
+	assert(args);
+
+	if(args == EMPTY_LIST) return scm_mk_error("No args passed to integer? (expected 1)");
+
+	Expr* fst = scm_car(args);
+	Expr* rst = scm_cdr(args);
+
+	if(rst != EMPTY_LIST) return scm_mk_error("Too many args passed to integer? (expected 1)");
+
+	return scm_is_int(fst) ? TRUE : FALSE;
+}
+
+static Expr* real(Expr* args) {
+	assert(args);
+
+	if(args == EMPTY_LIST) return scm_mk_error("No args passed to real? (expected 1)");
+
+	Expr* fst = scm_car(args);
+	Expr* rst = scm_cdr(args);
+
+	if(rst != EMPTY_LIST) return scm_mk_error("Too many args passed to real? (expected 1)");
+
+	return scm_is_real(fst) ? TRUE : FALSE;
+}
+
+static Expr* exact(Expr* args) {
+	assert(args);
+
+	if(args == EMPTY_LIST) return scm_mk_error("No args passed to exact? (expected 1)");
+
+	Expr* fst = scm_car(args);
+	Expr* rst = scm_cdr(args);
+
+	if(rst != EMPTY_LIST) return scm_mk_error("Too many args passed to exact? (expected 1)");
+	if(number(args) != TRUE) return scm_mk_error("Argument to exact? is not a number");
+
+	return scm_is_int(fst) ? TRUE : FALSE;
+}
+
+static Expr* inexact(Expr* args) {
+	assert(args);
+
+	if(args == EMPTY_LIST) return scm_mk_error("No args passed to inexact? (expected 1)");
+
+	Expr* fst = scm_car(args);
+	Expr* rst = scm_cdr(args);
+
+	if(rst != EMPTY_LIST) return scm_mk_error("Too many args passed to inexact? (expected 1)");
+	if(number(args) != TRUE) return scm_mk_error("Argument to inexact? is not a number");
+
+	return scm_is_real(fst) ? TRUE : FALSE;
+}
+
+static Expr* add(Expr* args) {
 	assert(args);
 
 	double dbuf = 0.0;
@@ -32,8 +99,24 @@ Expr* add(Expr* args) {
 }
 
 
-static Expr ADD = { .tag = ATOM, .atom = { .type = FFUNC, .ffptr = add }, .protect = true, .mark = true };
+#define mk_ff(name, ptr) static Expr name = { .tag = ATOM, .atom = { .type = FFUNC, .ffptr = ptr }, .protect = true, .mark = true }
+
+mk_ff(NUMBER, number);
+mk_ff(INTEGER, integer);
+mk_ff(REALL, real);
+mk_ff(EXACT, exact);
+mk_ff(INEXACT, inexact);
+
+mk_ff(ADD, add);
+
+#define bind_ff(name, oname) scm_env_define(BASE_ENV, scm_mk_symbol(name), &oname)
 
 void scm_init_func() {
-	scm_env_define(BASE_ENV, scm_mk_symbol("+"), &ADD);
+	bind_ff("number?", NUMBER);
+	bind_ff("integer?", INTEGER);
+	bind_ff("real?", REALL);
+	bind_ff("exact?", EXACT);
+	bind_ff("inexact?", INEXACT);
+
+	bind_ff("+", ADD);
 }
